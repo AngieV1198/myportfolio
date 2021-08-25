7b9040fec024121bec25dd72f79060ef1761e374
AngieV1198 <angievarela1998@gmail.com>
2021-08-25 17:10:32 -0500

    firscomic

--- js/music-list.js
+++ js/music-list.js
@@ -6,15 +6,15 @@ let allMusic = [
       src: "Melting"
     },
     {
+      name: "Soñé Contigo",
+      artist: "C. Sadness-M.Mares",
+      img: "c3",
+      src: "sonecontigo"
+    },
+    {
       name: "Yonaguni",
       artist: "Bad Bunny",
       img: "c2",
       src: "Yonaguni"
-    },
-    {
-      name: "Soñé Contigo",
-      artist: "C. Sadness-M.Mares",
-      img: "c3",
-      src: "Soñé Contigo"
-    },
+    }, 
   ];
