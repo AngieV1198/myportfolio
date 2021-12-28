4b5a9b55a6e0bccb58b2e33388d573696d3dd856
AngieV1198 <angievarela1998@gmail.com>
2021-12-28 17:45:48 -0500

    s

--- marketing.js
+++
@@ -1,7 +0,0 @@
-window.onscroll = function() {
-    var pos = document.documentElement.scrollTop;
-    var calc_height = document.documentElement.scrollHeight-
-                      document.documentElement.clientHeight;
-    var scroll = pos * 100 / calc_height;
-    document.getElementById("progress").style.width = scroll + "%";
-}
